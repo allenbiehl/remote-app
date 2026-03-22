843e0c68725b8071cfe9c3e9522a7b9780b91289
Evan Allen Biehl <biehl_evan@bah.com>
2026-03-22 18:16:54 -0400

    Update app

--- src/remote_app/app.py
+++ src/remote_app/app.py
@@ -15,9 +15,9 @@ class AppController:
         self._stop_event = stop_event
 
     def run(self) -> None:
-        logging.info("starting remote-app (version 1.0.2)")
+        logging.info("starting remote-app (version 1.0.4)")
         while not self._stop_event.is_set():
             logging.info("Running %s", datetime.now())
             time.sleep(30)
 
-        logging.info("stopping remote-app (version 1.0.2)")
+        logging.info("stopping remote-app (version 1.0.4)")
