b3686d7b01376899f675411476a617f4d6cf0aaf
Evan Allen Biehl <biehl_evan@bah.com>
2026-03-26 00:35:28 -0400

    Update version

--- src/remote_app/utils/logging_utils.py
+++ src/remote_app/utils/logging_utils.py
@@ -34,7 +34,7 @@ def create_logger(name: str, log_format: Optional[str] = None):
     """
     if not log_format:
         log_prefix = get_log_prefix()
-        log_format = f"{log_prefix}%(asctime)s - %(levelname)s - %(message)s"
+        log_format = f"::{log_prefix}%(asctime)s [%(levelname)s] %(name)s: %(message)s"
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
